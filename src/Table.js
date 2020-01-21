565b25998c9e9d4737ce18603949cfc2a8d85d6f
Danny Schoeter <danny.schoeter@xt-i.com>
2020-01-21 17:16:40 +0100

    Use state to modify data

--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ const TableHeader = () => {
                 <tr>
                     <th>Name</th>
                     <th>Job</th>
+                    <th>Remove</th>
                 </tr>
             </thead>
     )
@@ -18,6 +19,9 @@ const TableBody = props => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
@@ -28,11 +32,11 @@ const TableBody = props => {
 }
 class Table extends Component {
     render() {
-        const { characterData } = this.props;
+        const { characterData, removeCharacter } = this.props;
         return( 
             <table>
                 <TableHeader />
-                <TableBody characterData={characterData}/>
+                <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
             </table>
         )
     }
